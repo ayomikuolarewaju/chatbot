7b5e2f4b65747ad24dad8546d9b293e5c53c6058
ayomikuolarewaju <ayomiku@ymail.com>
2025-08-28 11:33:13 +0100

    second

--- src/actions/action.ts
+++ src/actions/action.ts
@@ -3,6 +3,12 @@ import { cookies } from 'next/headers'
 import { redirect } from 'next/navigation'
 import Groq from 'groq-sdk';
 
+const client = new Groq({
+  apiKey: process.env['GROQ_API_KEY'], // This is the default and can be omitted
+});
+
+
+
   export type FormState = {
   error: SubErrors
 }
@@ -41,11 +47,11 @@ import Groq from 'groq-sdk';
    try {
 
      const res = await client.chat.completions.create({
-      messages: [{ role: 'user', content: `Welcome ${name}, I asked about ${location} business viability. Write a short description about ${location} business viability.` }],
-      model: 'llama3-8b-8192',
-      });
+        messages: [{ role: 'user', content: 'Explain the importance of low latency LLMs' }],
+        model: 'llama3-8b-8192',
+        });
 
-   console.log(res.choices[0].message.content);
+         console.log(res.choices[0].message.content);
     
    } catch (error) {
     console.error('Fetch error:', error);
